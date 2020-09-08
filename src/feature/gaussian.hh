ab8377b117e1735df8b8cfefbdcbecf089a6ce95
Francisco J. Solis-Munoz <siscomagma@gmail.com>
2020-09-08 09:54:07 -0500

    Reverting changes

--- src/feature/gaussian.hh
+++ src/feature/gaussian.hh
@@ -9,30 +9,25 @@
 #include "lib/utils.hh"
 #include "lib/timer.hh"
 #include "common/common.hh"
-namespace pano
-{
+namespace pano {
 
-	class GaussCache
-	{
+class GaussCache {
 	public:
 		std::unique_ptr<float, std::default_delete<float[]>> kernel_buf;
-		float *kernel;
+		float* kernel;
 		int kw;
 		GaussCache(float sigma);
-	};
+};
 
-	class GaussianBlur
-	{
-		float sigma;
-		GaussCache gcache;
-
+class GaussianBlur {
+	float sigma;
+	GaussCache gcache;
 	public:
-		GaussianBlur(float sigma) : sigma(sigma), gcache(sigma) {}
+		GaussianBlur(float sigma): sigma(sigma), gcache(sigma) {}
 
 		// TODO faster convolution
 		template <typename T>
-		Mat<T> blur(const Mat<T> &img) const
-		{
+		Mat<T> blur(const Mat<T>& img) const {
 			m_assert(img.channels() == 1);
 			TotalTimer tm("gaussianblur");
 			const int w = img.width(), h = img.height();
@@ -40,35 +35,32 @@ namespace pano
 
 			const int kw = gcache.kw;
 			const int center = kw / 2;
-			float *kernel = gcache.kernel;
+			float * kernel = gcache.kernel;
 
 			std::vector<T> cur_line_mem(center * 2 + std::max(w, h), 0);
 			T *cur_line = cur_line_mem.data() + center;
 
 			// apply to columns
-			REP(j, w)
-			{
-				const T *src = img.ptr(0, j);
+			REP(j, w){
+				const T* src = img.ptr(0, j);
 				// copy a column of src
-				REP(i, h)
-				{
+				REP(i, h) {
 					cur_line[i] = *src;
 					src += w;
 				}
 
 				// pad the border with border value
 				T v0 = cur_line[0];
-				for (int i = 1; i <= center; i++)
+				for (int i = 1; i <= center; i ++)
 					cur_line[-i] = v0;
 				v0 = cur_line[h - 1];
-				for (int i = 0; i < center; i++)
+				for (int i = 0; i < center; i ++)
 					cur_line[h + i] = v0;
 
 				T *dest = ret.ptr(0, j);
-				REP(i, h)
-				{
+				REP(i, h) {
 					T tmp{0};
-					for (int k = -center; k <= center; k++)
+					for (int k = -center; k <= center; k ++)
 						tmp += cur_line[i + k] * kernel[k];
 					*dest = tmp;
 					dest += w;
@@ -76,49 +68,42 @@ namespace pano
 			}
 
 			// apply to rows
-			REP(i, h)
-			{
+			REP(i, h) {
 				T *dest = ret.ptr(i);
-				memcpy((void *)cur_line, (void *)dest, sizeof(T) * w);
-				{ // pad the border
+				memcpy(cur_line, dest, sizeof(T) * w);
+				{	// pad the border
 					T v0 = cur_line[0];
-					for (int j = 1; j <= center; j++)
+					for (int j = 1; j <= center; j ++)
 						cur_line[-j] = v0;
 					v0 = cur_line[w - 1];
-					for (int j = 0; j < center; j++)
+					for (int j = 0; j < center; j ++)
 						cur_line[w + j] = v0;
 				}
-				REP(j, w)
-				{
+				REP(j, w) {
 					T tmp{0};
-					for (int k = -center; k <= center; k++)
+					for (int k = -center; k <= center; k ++)
 						tmp += cur_line[j + k] * kernel[k];
-					*(dest++) = tmp;
+					*(dest ++) = tmp;
 				}
 			}
 			return ret;
 		}
-	};
+};
 
-	class MultiScaleGaussianBlur
-	{
-		std::vector<GaussianBlur> gauss; // size = nscale - 1
+class MultiScaleGaussianBlur {
+	std::vector<GaussianBlur> gauss;		// size = nscale - 1
 	public:
-		MultiScaleGaussianBlur(
+	MultiScaleGaussianBlur(
 			int nscale, float gauss_sigma,
-			float scale_factor)
-		{
-			REP(k, nscale - 1)
-			{
-				gauss.emplace_back(gauss_sigma);
-				gauss_sigma *= scale_factor;
-			}
+			float scale_factor) {
+		REP(k, nscale - 1) {
+			gauss.emplace_back(gauss_sigma);
+			gauss_sigma *= scale_factor;
 		}
+	}
 
-		Mat32f blur(const Mat32f &img, int n) const
-		{
-			return gauss[n - 1].blur(img);
-		}
-	};
+	Mat32f blur(const Mat32f& img, int n) const
+	{ return gauss[n - 1].blur(img); }
+};
 
-} // namespace pano
+}
