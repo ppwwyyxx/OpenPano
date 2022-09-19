c82c0a1d5e862bb5678c9080158e089de0e9d13e
Pei Jia <jiapei@longervision.com>
2022-09-19 03:26:53 -0700

    Compatible with Python 3, and generate a dynamic library rather than a static one.

--- src/run_test.py
+++ src/run_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 # File: run_test.py
 # Author: Yuxin Wu <ppwwyyxx@gmail.com>
@@ -18,12 +18,12 @@ def good_size(x_test, x_truth):
     return ratio > THRESHOLD
 
 def test_final_size(image_globs, w, h):
-    print "Testing with {}".format(image_globs)
+    print ("Testing with {}".format(image_globs))
     images = sorted(glob.glob(image_globs))
     cmd = [EXEC] + images
     outputs = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     outputs = outputs.split('\n')
-    print '\n'.join(outputs)
+    print ('\n'.join(outputs))
     for line in outputs:
         if 'Final Image Size' in line:
             m = re.match(r'.*\(([0-9]+), ([0-9]+)\)', line)
@@ -31,7 +31,7 @@ def test_final_size(image_globs, w, h):
             if good_size(ww, w) and good_size(hh, h):
                 return
             break
-    print "Test Failed!"
+    print ("Test Failed!")
     sys.exit(1)
 
 if __name__ == '__main__':
@@ -42,4 +42,4 @@ if __name__ == '__main__':
         assert ret == 0
     test_final_size('example-data/zijing/*', 6488, 1100)
     test_final_size('example-data/CMU1/*', 8000, 1449)
-    print "Tests Passed"
+    print ("Tests Passed")
